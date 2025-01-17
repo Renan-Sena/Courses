0d14d1595a820975514cfc738dfeefdac3b717cf
Renan Sena <renanlealsena00@gmail.com>
2025-01-17 17:07:02 -0400

    second

--- importa-json.js
+++
@@ -1,8 +0,0 @@
-// import estudante from './estudate.json'
-const estudante = require('./estudante.json');
-
-// console.log(estudante);
-// console.log(typeof estudante);
-
-const chaves = Object.entries(estudante);
-console.log(chaves);
